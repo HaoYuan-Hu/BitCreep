e940e3c52fdcf80a921a3d4515f606ad6710988f
chengke <chengke3@163.com>
2021-04-24 17:05:25 +0800

    backup

--- app/src/main/java/com/example/bitcreep/activity/VideoActivity.java
+++ app/src/main/java/com/example/bitcreep/activity/VideoActivity.java
@@ -2,13 +2,35 @@ package com.example.bitcreep.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 
+import com.example.bitcreep.R;
+import com.example.bitcreep.utils.Message;
+
+import android.widget.VideoView;
+
+
+
 public class VideoActivity extends AppCompatActivity {
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        String studentId = getIntent().getStringExtra("student_id");
+        String videoUrl = getIntent().getStringExtra("video_url");
+
+        videoView = findViewById(R.id.videoView);
+        videoView.getHolder().setFormat(PixelFormat.TRANSPARENT);
+        videoView.setZOrderOnTop(true);
+
+        videoView.setVideoPath(getVideoPath(R.raw.video1));
+    }
+    private String getVideoPath(int resId) {
+        return "android.resource://" + this.getPackageName() + "/" + resId;
     }
 }
