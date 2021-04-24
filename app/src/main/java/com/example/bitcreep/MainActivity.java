424f85c0622fde93eae26259824b58798e7d1d7f
HaoYuan-Hu <1124730278@qq.com>
2021-04-24 15:16:29 +0800

    tmp

--- app/src/main/java/com/example/bitcreep/MainActivity.java
+++ app/src/main/java/com/example/bitcreep/MainActivity.java
@@ -1,10 +1,28 @@
 package com.example.bitcreep;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "chapter5";
+    private FeedAdapter adapter = new FeedAdapter();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
